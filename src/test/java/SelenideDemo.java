23ba17d51ec0d525af0823e6a0fa5c7dc0d96a25
zaleg <ozaporozhets@intellias.com>
2017-11-14 13:16:15 +0200

    Screenshots to reports added.

--- src/test/java/SelenideDemo.java
+++ src/test/java/SelenideDemo.java
@@ -1,16 +1,26 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Screenshots;
+import com.google.common.io.Files;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
-import org.junit.Test;
 import org.openqa.selenium.By;
 import com.codeborne.selenide.WebDriverRunner;
 import org.openqa.selenium.Dimension;
+import org.testng.annotations.Test;
+import ru.yandex.qatools.allure.annotations.Attachment;
+import ru.yandex.qatools.allure.annotations.Description;
+import ru.yandex.qatools.allure.annotations.Step;
+
+import java.io.File;
+import java.io.IOException;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
 public class SelenideDemo {
+
+    @Description("Simple Selenide UI test.")
     @Test
-    public void ukrNetEmailBoxLoginTest () throws InterruptedException {
+    public void ukrNetEmailBoxLoginTest () throws InterruptedException, IOException {
         // ChromeDriver initialization
         ChromeDriverManager.getInstance().setup();
         // Setting Browser type instead Firefox by default
@@ -23,7 +33,20 @@ public class SelenideDemo {
         $(By.name("Password")).setValue("incorrect_pass");
         $(By.xpath("//*[@id=\"user-login-form\"]//button")).click();
         // Error message validation
-        System.out.println("Browser window size is: " + WebDriverRunner.getWebDriver().manage().window().getSize()); // test dimention changed.
-        $(By.className("error-text")).shouldHave(text("Неправильно вказано логін чи пароль. Спробуйте знову."));
+        System.out.println("Browser window size is: " + WebDriverRunner.getWebDriver().manage().window().getSize()); // test dimension changed.
+        validateMessage();
+    }
+
+    @Step("Message validation.")
+    private void validateMessage() throws IOException {
+        screenshot();
+        $(By.className("error-text")).shouldBe(visible)
+                .has(text("Неправильно вказано логін чи пароль. Спробуйте знову."));
+    }
+
+    @Attachment(type = "image/png")
+    private byte[] screenshot() throws IOException {
+        File screenshot = Screenshots.takeScreenShotAsFile();
+        return Files.toByteArray(screenshot);
     }
 }
